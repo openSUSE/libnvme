fa8062e8c33fd483faecd4d95b1837bfba72c29d
Martin Belanger <martin.belanger@dell.com>
2023-02-27 13:37:20 -0500

    python: Fix segmentation fault during garbage collection
    With SWIG we create proxy classes to wrap the C structures (struct).
    These classes are used to create Python objects matching the C
    structures such as:
    
    `nvme.root` is used to wrap `struct nvme_root`
    `nvme.host` is used to wrap `struct nvme_host`
    `nvme.ctrl` is used to wrap `struct nvme_ctrl`
    etc...
    
    One thing that SWIG cannot do is figure out the dependencies
    between the different objects. For example, it cannot tell that
    when deleting a host object that all the subsystems, controllers,
    namespaces under that host need to be deleted as well. That's an
    implicit property of the libnvme driver and users need to know to
    stop using objects after their parent has been deleted.
    
    Unfotunately, with Python the Gargage Collector (GC) decides which
    objects to delete and it can delete obects in any order it sees fit.
    This can result in objects being deleted in the wrong order. For
    example, the GC may delete a host object (`nvme_free_host`) before
    deleting any of the controller objects under that host. And when the
    GC finally deletes a controller by calling `nvme_free_ctrl()`, the
    memory for that controller has already been freed and a segmentation
    fault will result.
    
    To enforce that objects get deleted in the right order, we need to
    set dependencies between objects at the Python level. This can be
    achived by having children objects maintain a reference to their
    parents. This way a parent cannot be deleted before all its children
    have been deleted.
    
    Signed-off-by: Martin Belanger <martin.belanger@dell.com>
    (cherry picked from commit 071bf6756e36484c983bf9e92bf35913ac45c39f)
    Signed-off-by: Daniel Wagner <dwagner@suse.de>

--- src/nvme/tree.c
+++ src/nvme/tree.c
@@ -349,6 +349,7 @@ static void __nvme_free_ns(struct nvme_ns *n)
 /* Stub for SWIG */
 void nvme_free_ns(struct nvme_ns *n)
 {
+	__nvme_free_ns(n);
 }
 
 static void __nvme_free_subsystem(struct nvme_subsystem *s)
@@ -444,6 +445,7 @@ static void __nvme_free_host(struct nvme_host *h)
 /* Stub for SWIG */
 void nvme_free_host(struct nvme_host *h)
 {
+	__nvme_free_host(h);
 }
 
 struct nvme_host *nvme_lookup_host(nvme_root_t r, const char *hostnqn,
