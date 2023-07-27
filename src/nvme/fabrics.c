linux-nvme/nvme-cli

fabrics: Add nvmf_get_discovery_wargs()
This function is similar to nvmf_get_discovery_log(), but takes
an extensible "args" parameter. The args parameter contains more
options than are available with nvmf_get_discovery_log(). For
example, it provides a way to set the LSP (Log page Specific field)
which is needed for advanced TP8010 features.

Signed-off-by: Martin Belanger <martin.belanger@dell.com>

src/nvme/fabrics.c
@@ -780,30 +780,9 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 	return NULL;
 }
 
-static int nvme_discovery_log(int fd, __u32 len, struct nvmf_discovery_log *log, bool rae)
-{
-	struct nvme_get_log_args args = {
-		.args_size = sizeof(args),
-		.fd = fd,
-		.nsid = NVME_NSID_NONE,
-		.lsp = NVME_LOG_LSP_NONE,
-		.lsi = NVME_LOG_LSI_NONE,
-		.uuidx = NVME_UUID_NONE,
-		.timeout = NVME_DEFAULT_IOCTL_TIMEOUT,
-		.result = NULL,
-		.lid = NVME_LOG_LID_DISCOVER,
-		.log = log,
-		.len = len,
-		.csi = NVME_CSI_NVM,
-		.rae = rae,
-		.ot = false,
-	};
-
-	return nvme_get_log_page(fd, 4096, &args);
-}
-
-int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
-			   int max_retries)
+static struct nvmf_discovery_log *nvme_discovery_log(nvme_ctrl_t c,
+						     struct nvme_get_log_args *args,
+						     int max_retries)
 {
 	nvme_root_t r = c->s && c->s->h ? c->s->h->r : NULL;
 	struct nvmf_discovery_log *log = NULL;
@@ -811,6 +790,9 @@ int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
 	const char *name = nvme_ctrl_get_name(c);
 	uint64_t genctr, numrec;
 	unsigned int size;
+	int fd = nvme_ctrl_get_fd(c);
+
+	args->fd = fd;
 
 	do {
 		size = sizeof(struct nvmf_discovery_log);
@@ -821,12 +803,15 @@ int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
 			nvme_msg(r, LOG_ERR,
 				 "could not allocate memory for discovery log header\n");
 			errno = ENOMEM;
-			return -1;
+			return NULL;
 		}
 
 		nvme_msg(r, LOG_DEBUG, "%s: get header (try %d/%d)\n",
 			name, retries, max_retries);
-		ret = nvme_discovery_log(nvme_ctrl_get_fd(c), size, log, true);
+		args->rae = true;
+		args->len = size;
+		args->log = log;
+		ret = nvme_get_log_page(fd, 4096, args);
 		if (ret) {
 			nvme_msg(r, LOG_INFO,
 				 "%s: discover try %d/%d failed, error %d\n",
@@ -849,14 +834,19 @@ int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
 			nvme_msg(r, LOG_ERR,
 				 "could not alloc memory for discovery log page\n");
 			errno = ENOMEM;
-			return -1;
+			return NULL;
 		}
 
 		nvme_msg(r, LOG_DEBUG,
 			 "%s: get header and %" PRIu64
 			 " records (length %d genctr %" PRIu64 ")\n",
 			 name, numrec, size, genctr);
-		ret = nvme_discovery_log(nvme_ctrl_get_fd(c), size, log, false);
+
+		args->rae = false;
+		args->len = size;
+		args->log = log;
+		ret = nvme_get_log_page(fd, 4096, args);
+
 		if (ret) {
 			nvme_msg(r, LOG_INFO,
 				 "%s: discover try %d/%d failed, error %d\n",
@@ -869,21 +859,63 @@ int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
 	if (genctr != le64_to_cpu(log->genctr)) {
 		nvme_msg(r, LOG_INFO, "%s: discover genctr mismatch\n", name);
 		errno = EAGAIN;
-		ret = -1;
 	} else if (numrec != le64_to_cpu(log->numrec)) {
 		nvme_msg(r, LOG_INFO,
 			 "%s: could only fetch %" PRIu64 " of %" PRIu64 " records\n",
 			 name, numrec, le64_to_cpu(log->numrec));
 		errno = EBADSLT;
-		ret = -1;
 	} else {
-		*logp = log;
-		return 0;
+		return log;
 	}
 
 out_free_log:
 	free(log);
-	return ret;
+	return NULL;
+}
+
+int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
+			   int max_retries)
+{
+	struct nvme_get_log_args args = {
+		.args_size = sizeof(args),
+		.fd = nvme_ctrl_get_fd(c),
+		.nsid = NVME_NSID_NONE,
+		.lsp = NVMF_LOG_DISC_LSP_NONE,
+		.lsi = NVME_LOG_LSI_NONE,
+		.uuidx = NVME_UUID_NONE,
+		.timeout = NVME_DEFAULT_IOCTL_TIMEOUT,
+		.result = NULL,
+		.lid = NVME_LOG_LID_DISCOVER,
+		.log = NULL,
+		.len = 0,
+		.csi = NVME_CSI_NVM,
+		.rae = false,
+		.ot = false,
+	};
+	*logp = nvme_discovery_log(c, &args, max_retries);
+	return logp ? 0 : -1;
+}
+
+struct nvmf_discovery_log *nvmf_get_discovery_wargs(struct nvme_get_discovery_args *args)
+{
+	struct nvme_get_log_args _args = {
+		.args_size = sizeof(_args),
+		.fd = nvme_ctrl_get_fd(args->c),
+		.nsid = NVME_NSID_NONE,
+		.lsp = args->lsp,
+		.lsi = NVME_LOG_LSI_NONE,
+		.uuidx = NVME_UUID_NONE,
+		.timeout = args->timeout,
+		.result = args->result,
+		.lid = NVME_LOG_LID_DISCOVER,
+		.log = NULL,
+		.len = 0,
+		.csi = NVME_CSI_NVM,
+		.rae = false,
+		.ot = false,
+	};
+
+	return nvme_discovery_log(args->c, &_args, args->max_retries);
 }
 
 #define PATH_UUID_IBM	"/proc/device-tree/ibm,partition-uuid"

openSUSE/libnvme

(cherry picked from commit ceb1aa3083f0e4603ce94935fdd6155324dd1e7e)
Signed-off-by: Daniel Wagner <dwagner@suse.de>

src/nvme/fabrics.c
@@ -797,30 +797,9 @@ static void sanitize_discovery_log_entry(struct nvmf_disc_log_entry  *e)
 	}
 }
 
-static int nvme_discovery_log(int fd, __u32 len, struct nvmf_discovery_log *log, bool rae)
-{
-	struct nvme_get_log_args args = {
-		.args_size = sizeof(args),
-		.fd = fd,
-		.nsid = NVME_NSID_NONE,
-		.lsp = NVME_LOG_LSP_NONE,
-		.lsi = NVME_LOG_LSI_NONE,
-		.uuidx = NVME_UUID_NONE,
-		.timeout = NVME_DEFAULT_IOCTL_TIMEOUT,
-		.result = NULL,
-		.lid = NVME_LOG_LID_DISCOVER,
-		.log = log,
-		.len = len,
-		.csi = NVME_CSI_NVM,
-		.rae = rae,
-		.ot = false,
-	};
-
-	return nvme_get_log_page(fd, 4096, &args);
-}
-
-int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
-			   int max_retries)
+static struct nvmf_discovery_log *nvme_discovery_log(nvme_ctrl_t c,
+						     struct nvme_get_log_args *args,
+						     int max_retries)
 {
 	nvme_root_t r = c->s && c->s->h ? c->s->h->r : NULL;
 	struct nvmf_discovery_log *log = NULL;
@@ -828,6 +807,9 @@ int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
 	const char *name = nvme_ctrl_get_name(c);
 	uint64_t genctr, numrec;
 	unsigned int size;
+	int fd = nvme_ctrl_get_fd(c);
+
+	args->fd = fd;
 
 	do {
 		size = sizeof(struct nvmf_discovery_log);
@@ -838,12 +820,15 @@ int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
 			nvme_msg(r, LOG_ERR,
 				 "could not allocate memory for discovery log header\n");
 			errno = ENOMEM;
-			return -1;
+			return NULL;
 		}
 
 		nvme_msg(r, LOG_DEBUG, "%s: get header (try %d/%d)\n",
 			name, retries, max_retries);
-		ret = nvme_discovery_log(nvme_ctrl_get_fd(c), size, log, true);
+		args->rae = true;
+		args->len = size;
+		args->log = log;
+		ret = nvme_get_log_page(fd, 4096, args);
 		if (ret) {
 			nvme_msg(r, LOG_INFO,
 				 "%s: discover try %d/%d failed, error %d\n",
@@ -866,14 +851,19 @@ int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
 			nvme_msg(r, LOG_ERR,
 				 "could not alloc memory for discovery log page\n");
 			errno = ENOMEM;
-			return -1;
+			return NULL;
 		}
 
 		nvme_msg(r, LOG_DEBUG,
 			 "%s: get header and %" PRIu64
 			 " records (length %d genctr %" PRIu64 ")\n",
 			 name, numrec, size, genctr);
-		ret = nvme_discovery_log(nvme_ctrl_get_fd(c), size, log, false);
+
+		args->rae = false;
+		args->len = size;
+		args->log = log;
+		ret = nvme_get_log_page(fd, 4096, args);
+
 		if (ret) {
 			nvme_msg(r, LOG_INFO,
 				 "%s: discover try %d/%d failed, error %d\n",
@@ -886,24 +876,63 @@ int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
 	if (genctr != le64_to_cpu(log->genctr)) {
 		nvme_msg(r, LOG_INFO, "%s: discover genctr mismatch\n", name);
 		errno = EAGAIN;
-		ret = -1;
 	} else if (numrec != le64_to_cpu(log->numrec)) {
 		nvme_msg(r, LOG_INFO,
 			 "%s: could only fetch %" PRIu64 " of %" PRIu64 " records\n",
 			 name, numrec, le64_to_cpu(log->numrec));
 		errno = EBADSLT;
-		ret = -1;
 	} else {
-		for (int i = 0; i < le64_to_cpu(log->numrec); i++)
-			sanitize_discovery_log_entry(&log->entries[i]);
-
-		*logp = log;
-		return 0;
+		return log;
 	}
 
 out_free_log:
 	free(log);
-	return ret;
+	return NULL;
+}
+
+int nvmf_get_discovery_log(nvme_ctrl_t c, struct nvmf_discovery_log **logp,
+			   int max_retries)
+{
+	struct nvme_get_log_args args = {
+		.args_size = sizeof(args),
+		.fd = nvme_ctrl_get_fd(c),
+		.nsid = NVME_NSID_NONE,
+		.lsp = NVME_LOG_LSP_NONE,
+		.lsi = NVME_LOG_LSI_NONE,
+		.uuidx = NVME_UUID_NONE,
+		.timeout = NVME_DEFAULT_IOCTL_TIMEOUT,
+		.result = NULL,
+		.lid = NVME_LOG_LID_DISCOVER,
+		.log = NULL,
+		.len = 0,
+		.csi = NVME_CSI_NVM,
+		.rae = false,
+		.ot = false,
+	};
+	*logp = nvme_discovery_log(c, &args, max_retries);
+	return logp ? 0 : -1;
+}
+
+struct nvmf_discovery_log *nvmf_get_discovery_wargs(struct nvme_get_discovery_args *args)
+{
+	struct nvme_get_log_args _args = {
+		.args_size = sizeof(_args),
+		.fd = nvme_ctrl_get_fd(args->c),
+		.nsid = NVME_NSID_NONE,
+		.lsp = args->lsp,
+		.lsi = NVME_LOG_LSI_NONE,
+		.uuidx = NVME_UUID_NONE,
+		.timeout = args->timeout,
+		.result = args->result,
+		.lid = NVME_LOG_LID_DISCOVER,
+		.log = NULL,
+		.len = 0,
+		.csi = NVME_CSI_NVM,
+		.rae = false,
+		.ot = false,
+	};
+
+	return nvme_discovery_log(args->c, &_args, args->max_retries);
 }
 
 #define PATH_UUID_IBM	"/proc/device-tree/ibm,partition-uuid"