linux-nvme/nvme-cli

fabrics: add error if --tls and --concat are invoked together
--tls and --concat are mutually exclusive and not meant to be
invoked together. So add an appropriate error message for
the same.

Signed-off-by: Martin George <marting@netapp.com>

libnvme/src/nvme/fabrics.c
@@ -732,6 +732,11 @@ static int build_options(nvme_host_t h, nvme_ctrl_t c, char **argstr)
 
 	ctrlkey = nvme_ctrl_get_dhchap_key(c);
 
+	if (cfg->tls && cfg->concat) {
+		nvme_msg(h->ctx, LOG_ERR, "cannot specify --tls and --concat together\n");
+		return -ENVME_CONNECT_INVAL;
+	}
+
 	if (cfg->tls) {
 		ret = __nvme_import_keys_from_config(h, c, &keyring_id, &key_id);
 		if (ret)

openSUSE/libnvme

(cherry picked from nvme-cli commit 75cfffcdf892c6cda1452e97da3598ec01489fa3)
Signed-off-by: Daniel Wagner <dwagner@suse.de>

src/nvme/fabrics.c
@@ -627,6 +627,11 @@ static int build_options(nvme_host_t h, nvme_ctrl_t c, char **argstr)
 
 	ctrlkey = nvme_ctrl_get_dhchap_key(c);
 
+	if (cfg->tls && cfg->concat) {
+		nvme_msg(h->r, LOG_ERR, "cannot specify --tls and --concat together\n");
+		return -ENVME_CONNECT_INVAL;
+	}
+
 	ret = __nvme_import_keys_from_config(h, c, &keyring_id, &key_id);
 	if (ret) {
 		errno = -ret;