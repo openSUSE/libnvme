linux-nvme/nvme-cli

fabrics: add additional debug messages for --tls and --concat
Add additional debug messages when --tls and --concat is set
respectively based on the treq and sectype fields for a given record
in the discovery log page data during a nvme connect-all. This makes
it easier to debug, otherwise can be quite confusing to the end user.

Signed-off-by: Martin George <marting@netapp.com>

libnvme/src/nvme/fabrics.c
@@ -1209,10 +1209,15 @@ static int nvmf_connect_disc_entry(nvme_host_t h,
 
 	if (e->trtype == NVMF_TRTYPE_TCP &&
 	    e->tsas.tcp.sectype != NVMF_TCP_SECTYPE_NONE) {
-		if (e->treq & NVMF_TREQ_REQUIRED)
+		if (e->treq & NVMF_TREQ_REQUIRED) {
+			nvme_msg(h->ctx, LOG_DEBUG, "setting --tls due to treq %s and sectype %s\n",
+					nvmf_treq_str(e->treq), nvmf_sectype_str(e->tsas.tcp.sectype));
 			c->cfg.tls = true;
-		else if (e->treq & NVMF_TREQ_NOT_REQUIRED)
+		} else if (e->treq & NVMF_TREQ_NOT_REQUIRED) {
+			nvme_msg(h->ctx, LOG_DEBUG, "setting --concat due to treq %s and sectype %s\n",
+					nvmf_treq_str(e->treq), nvmf_sectype_str(e->tsas.tcp.sectype));
 			c->cfg.concat = true;
+		}
 	}
 
 	ret = nvmf_add_ctrl(h, c, cfg);

openSUSE/libnvme

(cherry picked from nvme-cli commit f0dd975155f47361f6ee230298b82fe94582f48c)
Signed-off-by: Daniel Wagner <dwagner@suse.de>

src/nvme/fabrics.c
@@ -1114,10 +1114,15 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 
 	if (e->trtype == NVMF_TRTYPE_TCP &&
 	    e->tsas.tcp.sectype != NVMF_TCP_SECTYPE_NONE) {
-		if (e->treq & NVMF_TREQ_REQUIRED)
+		if (e->treq & NVMF_TREQ_REQUIRED) {
+			nvme_msg(h->r, LOG_DEBUG, "setting --tls due to treq %s and sectype %s\n",
+					nvmf_treq_str(e->treq), nvmf_sectype_str(e->tsas.tcp.sectype));
 			c->cfg.tls = true;
-		else if (e->treq & NVMF_TREQ_NOT_REQUIRED)
+		} else if (e->treq & NVMF_TREQ_NOT_REQUIRED) {
+			nvme_msg(h->r, LOG_DEBUG, "setting --concat due to treq %s and sectype %s\n",
+					nvmf_treq_str(e->treq), nvmf_sectype_str(e->tsas.tcp.sectype));
 			c->cfg.concat = true;
+		}
 	}
 
 	ret = nvmf_add_ctrl(h, c, cfg);