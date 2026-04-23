linux-nvme/nvme-cli

fabrics: add helper to update tls and concat
Only --tls was properly updated in nbft_connect(), and not
--concat. But this is properly done in nvmf_connect_disc_entry()
already. So add a helper function to update both --tls and
--concat and invoke the same from nvmf_connect_disc_entry() and
nbft_connect() respectively.

Signed-off-by: Martin George <marting@netapp.com>
[wagi: reformated the function to improve readability]
Signed-off-by: Daniel Wagner <wagi@kernel.org>

libnvme/src/nvme/fabrics.c
@@ -1096,6 +1096,34 @@ __public int nvmf_connect_ctrl(nvme_ctrl_t c)
 	return 0;
 }
 
+static void nvmf_update_tls_concat(struct nvmf_disc_log_entry *e,
+		nvme_ctrl_t c, nvme_host_t h)
+{
+	if (e->trtype != NVMF_TRTYPE_TCP ||
+	    e->tsas.tcp.sectype == NVMF_TCP_SECTYPE_NONE)
+		return;
+
+	if (e->treq & NVMF_TREQ_REQUIRED) {
+		nvme_msg(h->ctx, LOG_DEBUG,
+			"setting --tls due to treq %s and sectype %s\n",
+			nvmf_treq_str(e->treq),
+			nvmf_sectype_str(e->tsas.tcp.sectype));
+
+		c->cfg.tls = true;
+		return;
+	}
+
+	if (e->treq & NVMF_TREQ_NOT_REQUIRED) {
+		nvme_msg(h->ctx, LOG_DEBUG,
+			"setting --concat due to treq %s and sectype %s\n",
+			nvmf_treq_str(e->treq),
+			nvmf_sectype_str(e->tsas.tcp.sectype));
+
+		c->cfg.concat = true;
+		return;
+	}
+}
+
 static int nvmf_connect_disc_entry(nvme_host_t h,
 		struct nvmf_disc_log_entry *e,
 		struct nvmf_context *fctx,
@@ -1189,18 +1217,8 @@ static int nvmf_connect_disc_entry(nvme_host_t h,
 	    nvmf_check_option(h->ctx, disable_sqflow))
 		c->cfg.disable_sqflow = true;
 
-	if (e->trtype == NVMF_TRTYPE_TCP &&
-	    e->tsas.tcp.sectype != NVMF_TCP_SECTYPE_NONE) {
-		if (e->treq & NVMF_TREQ_REQUIRED) {
-			nvme_msg(h->ctx, LOG_DEBUG, "setting --tls due to treq %s and sectype %s\n",
-					nvmf_treq_str(e->treq), nvmf_sectype_str(e->tsas.tcp.sectype));
-			c->cfg.tls = true;
-		} else if (e->treq & NVMF_TREQ_NOT_REQUIRED) {
-			nvme_msg(h->ctx, LOG_DEBUG, "setting --concat due to treq %s and sectype %s\n",
-					nvmf_treq_str(e->treq), nvmf_sectype_str(e->tsas.tcp.sectype));
-			c->cfg.concat = true;
-		}
-	}
+	/* update tls or concat */
+	nvmf_update_tls_concat(e, c, h);
 
 	ret = nvmf_add_ctrl(h, c, cfg);
 	if (!ret) {
@@ -2552,11 +2570,8 @@ static int nbft_connect(struct nvme_global_ctx *ctx,
 	if (ss && ss->unavailable && saved_log_level < 1)
 		nvme_set_logging_level(ctx, -1, false, false);
 
-	if (e) {
-		if (e->trtype == NVMF_TRTYPE_TCP &&
-		    e->tsas.tcp.sectype != NVMF_TCP_SECTYPE_NONE)
-			cfg->tls = true;
-	}
+	/* Update tls or concat */
+	nvmf_update_tls_concat(e, c, h);
 
 	ret = nvmf_add_ctrl(h, c, cfg);
 

openSUSE/libnvme

(cherry picked from 99dd46fdff25)
(cherry picked from commit 085b90bdda62371a119799589e9ef9fab144ce6e)
Signed-off-by: Daniel Wagner <dwagner@suse.de>

src/nvme/fabrics.c
@@ -1016,6 +1016,34 @@ int nvmf_connect_ctrl(nvme_ctrl_t c)
 	return 0;
 }
 
+static void nvmf_update_tls_concat(struct nvmf_disc_log_entry *e,
+		nvme_ctrl_t c, nvme_host_t h)
+{
+	if (e->trtype != NVMF_TRTYPE_TCP ||
+	    e->tsas.tcp.sectype == NVMF_TCP_SECTYPE_NONE)
+		return;
+
+	if (e->treq & NVMF_TREQ_REQUIRED) {
+		nvme_msg(h->r, LOG_DEBUG,
+			"setting --tls due to treq %s and sectype %s\n",
+			nvmf_treq_str(e->treq),
+			nvmf_sectype_str(e->tsas.tcp.sectype));
+
+		c->cfg.tls = true;
+		return;
+	}
+
+	if (e->treq & NVMF_TREQ_NOT_REQUIRED) {
+		nvme_msg(h->r, LOG_DEBUG,
+			"setting --concat due to treq %s and sectype %s\n",
+			nvmf_treq_str(e->treq),
+			nvmf_sectype_str(e->tsas.tcp.sectype));
+
+		c->cfg.concat = true;
+		return;
+	}
+}
+
 nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 				    struct nvmf_disc_log_entry *e,
 				    const struct nvme_fabrics_config *cfg,
@@ -1114,18 +1142,8 @@ nvme_ctrl_t nvmf_connect_disc_entry(nvme_host_t h,
 	    nvmf_check_option(h->r, disable_sqflow))
 		c->cfg.disable_sqflow = true;
 
-	if (e->trtype == NVMF_TRTYPE_TCP &&
-	    e->tsas.tcp.sectype != NVMF_TCP_SECTYPE_NONE) {
-		if (e->treq & NVMF_TREQ_REQUIRED) {
-			nvme_msg(h->r, LOG_DEBUG, "setting --tls due to treq %s and sectype %s\n",
-					nvmf_treq_str(e->treq), nvmf_sectype_str(e->tsas.tcp.sectype));
-			c->cfg.tls = true;
-		} else if (e->treq & NVMF_TREQ_NOT_REQUIRED) {
-			nvme_msg(h->r, LOG_DEBUG, "setting --concat due to treq %s and sectype %s\n",
-					nvmf_treq_str(e->treq), nvmf_sectype_str(e->tsas.tcp.sectype));
-			c->cfg.concat = true;
-		}
-	}
+	/* update tls or concat */
+	nvmf_update_tls_concat(e, c, h);
 
 	ret = nvmf_add_ctrl(h, c, cfg);
 	if (!ret)